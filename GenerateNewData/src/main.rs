a8b316577d05f9e1fd13af34587bdc36490938bc
sdrafahl <sdrafahl@vagrant.jdnet.deere.com>
2019-10-24 17:07:40 -0500

    Added rust code to generate test avro data and also began on dag structure or transformation.

--- GenerateNewData/src/main.rs
+++ GenerateNewData/src/main.rs
@@ -3,54 +3,70 @@ extern crate avro_rs;
 #[macro_use]
 extern crate serde_derive;
 extern crate failure;
+extern crate base64;
 
-use avro_rs::{Codec, Reader, Schema, Writer, from_value, types::Record};
+use avro_rs::{Codec, Schema, Writer, from_value, Reader, types::Record};
 use failure::Error;
+use std::vec::Vec;
+use std::fs;
+use std::io;
+use base64::{encode, decode};
+use parity_snappy as snappy;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Test {
    a: i64,
-    b: String,
+   b: String,
+   link: String 
 }
 
 fn main() -> Result<(), Error> {
+    
     let raw_schema = r#"
         {
             "type": "record",
             "name": "test",
             "fields": [
                 {"name": "a", "type": "long", "default": 42},
-                {"name": "b", "type": "string"}
+                {"name": "b", "type": "string"},
+                {"name": "link", "type": "string"}
             ]
         }
     "#;
 
     let schema = Schema::parse_str(raw_schema)?;
     
-    println!("{:?}", schema);
-
-    let mut writer = Writer::with_codec(&schema, Vec::new(), Codec::Deflate);
-
+    //let mut writer = Writer::with_codec(&schema, Vec::new(), Codec::Snappy);
+    let mut writer = Writer::with_codec(&schema, Vec::new(), Codec::Null);
     let mut record = Record::new(writer.schema()).unwrap();
     record.put("a", 27i64);
     record.put("b", "foo");
+    record.put("link", "www.google.com");
 
     writer.append(record)?;
 
-    let test = Test {
-        a: 27,
-        b: "foo".to_owned(),
-    };
-
-    writer.append_ser(test)?;
-
     writer.flush()?;
-
+    
     let input = writer.into_inner();
-    let reader = Reader::with_schema(&schema, &input[..])?;
+    let comped = snappy::compress(&input);
+    print_to_file(comped);
+    read_avro_file(schema);
+    Ok(())
+}
 
+fn read_avro_file(schema: Schema) -> Result<(), Error> {
+    let compressedb64 = fs::read("avroData")?;
+    let compressed = decode(&compressedb64)?;
+    let decompressed = snappy::decompress(&compressed)?;
+    let reader = Reader::with_schema(&schema, &decompressed[..])?;
     for record in reader {
         println!("{:?}", from_value::<Test>(&record?));
     }
     Ok(())
+} 
+
+fn print_to_file(avroData: Vec<u8>) {
+    let base64Data = encode(&avroData);
+    fs::write("avroData", &base64Data);
 }
+
--- infrastructure/EtlDemo.tf
+++ infrastructure/EtlDemo.tf
@@ -1,3 +1,56 @@
+variable "CrawlerRole" {
+  type = string
+  default = "arn:aws:iam::061753407487:role/Glue"
+}
+
+resource "aws_s3_bucket" "test_avro_data_source" {
+  bucket = "testavrodatasource"
+  acl    = "public-read-write"
+}
+
+resource "aws_s3_bucket_object" "file_upload_testdata" {
+  bucket = "${aws_s3_bucket.test_avro_data_source.bucket}"
+  key = "GenerateNewData"
+  source = "${path.module}/../GenerateNewData/target/debug/GenerateNewData"
+}
+
+resource "aws_dynamodb_table" "test_avro_data_source_index" {
+  name           = "testindex"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+  range_key      = "s3location"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "s3location"
+    type = "S"
+  }
+  
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+}
+
+
+resource "aws_dynamodb_table_item" "example" {
+  table_name = "${aws_dynamodb_table.test_avro_data_source_index.name}"
+  hash_key   = "${aws_dynamodb_table.test_avro_data_source_index.hash_key}"
+
+  item = <<ITEM
+{
+  "id": {"S": "1"},
+  "s3location": {"S": ${aws_s3_bucket_object.file_upload.key}"}
+}
+ITEM
+}
+
 variable "GlueCatalogDatabaseName" {
   type = string
   default = "demogluedatabase"
@@ -6,10 +59,6 @@ variable "GlueCatalogDatabaseName" {
 variable "CrawlerRole" {
   type = string
   default = "arn:aws:iam::061753407487:role/Glue"
-}
-
-variable "DynamoTableName" {
-  type = string
 }
 
 variable "S3BucketName" {
@@ -25,8 +74,135 @@ resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
   name = var.GlueCatalogDatabaseName
 }
 
+resource "aws_glue_crawler" "dynamo_crawler" {
+  database_name = "${aws_dynamodb_table.test_avro_data_source_index.name}"
+  name          = "dynamoCrawler"
+  role          = var.CrawlerRole
+  schedule      = "cron(15 12 * * ? *)"
 
+  dynamodb_target {
+    path = var.DynamoTableName
+  }
+}
 
+resource "aws_glue_crawler" "example" {
+  database_name = "${aws_dynamodb_table.test_avro_data_source_index.name}"
+  name          = "s3Crawler"
+  role          = var.CrawlerRole
 
+  s3_target {
+    path = "s3://${aws_s3_bucket.test_avro_data_source}/${aws_s3_bucket_object.file_upload.key}"
+  }
+}
+
+data "aws_glue_script" "scala_script" {
+  language = "SCALA"
+
+  dag_node {
+    id        = "datasourceDynamo"
+    node_type = "DataSource"
+
+    args {
+      name  = "database"
+      value = "\"${aws_glue_catalog_database.aws_glue_catalog_database.name}\""
+    }
+
+    args {
+      name  = "table_name"
+      value = "\"${aws_dynamodb_table.test_avro_data_source_index.name}\""
+    }
+  }
+
+  dag_node {
+    id        = "datasourceS3"
+    node_type = "DataSource"
+
+    args {
+      name  = "database"
+      value = "\"${aws_glue_catalog_database.aws_glue_catalog_database.name}\""
+    }
+
+    args {
+      name  = "table_name"
+      value = "\"${aws_s3_bucket.test_avro_data_source.bucket}\""
+    }
+  }
+
+  dag_node {
+    id        = "joinDynamoAndS3"
+    node_type = "Join"
+
+    args {
+      name  = "frame1"
+      value = "datasourceS3"
+    }
+
+    args {
+      name  = "frame2"
+      value = "datasourceDynamo"
+    }
+
+    args {
+      name  = "keys1"
+      value = "[\"${aws_s3_bucket_object.file_upload_testdata.key}\"]"
+    }
+
+    args {
+      name = "keys2"
+      value = "[\"${aws_dynamodb_table.test_avro_data_source_index.hash_key}\", \"${aws_dynamodb_table.test_avro_data_source_index.range_key}\"]"
+    }
+  }
+  
+  dag_node {
+    id        = "resolvechoice"
+    node_type = "ResolveChoice"
+
+    args {
+      name  = "choice"
+      value = "\"MATCH_CATALOG\""
+    }
+
+    args {
+      name  = "database"
+      value = "\"${aws_glue_catalog_database.aws_glue_catalog_database_destination.name}\""
+    }
+
+    args {
+      name  = "table_name"
+      value = "\"dest\""
+    }
+  }
+
+  dag_node {
+    id        = "datasink5"
+    node_type = "DataSink"
+
+    args {
+      name  = "database"
+      value = "\"${aws_glue_catalog_database.aws_glue_catalog_database_destination.name}\""
+    }
+
+    args {
+      name  = "table_name"
+      value = "\"${aws_glue_catalog_table.aws_glue_catalog_table_destination.name}\""
+    }
+  }
+}
+
+resource "aws_s3_bucket" "scala_dag" {
+  bucket = "shanesscaladag"
+  acl    = "public-read-write"
+}
+
+resource "local_file" "scala_code" {
+  content  = "${data.aws_glue_script.scala_script.scala_code}"
+  filename = "${path.module}/scalaCode.scala"
+}
+
+resource "aws_s3_bucket_object" "file_upload" {
+  bucket = "${aws_s3_bucket.scala_dag.bucket}"
+  key = "transform.scala"
+  source = "${local_file.scala_code.filename}"
+}
 
 
