99878daa3a5e3c01948815188f09cdc31f44b32e
sdrafahl <sdrafahl@vagrant.jdnet.deere.com>
2019-10-27 17:01:22 -0500

    moving stuff and added more to the demo

--- infrastructure/EtlDemo.tf
+++ infrastructure/EtlDemo.tf
@@ -1,8 +1,3 @@
-variable "CrawlerRole" {
-  type = string
-  default = "arn:aws:iam::061753407487:role/Glue"
-}
-
 resource "aws_s3_bucket" "test_avro_data_source" {
   bucket = "testavrodatasource"
   acl    = "public-read-write"
@@ -17,10 +12,9 @@ resource "aws_s3_bucket_object" "file_upload_testdata" {
 resource "aws_dynamodb_table" "test_avro_data_source_index" {
   name           = "testindex"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
+  read_capacity  = 5
+  write_capacity = 5
   hash_key       = "id"
-  range_key      = "s3location"
 
   attribute {
     name = "id"
@@ -38,31 +32,14 @@ resource "aws_dynamodb_table" "test_avro_data_source_index" {
   }
 }
 
-
-resource "aws_dynamodb_table_item" "example" {
-  table_name = "${aws_dynamodb_table.test_avro_data_source_index.name}"
-  hash_key   = "${aws_dynamodb_table.test_avro_data_source_index.hash_key}"
-
-  item = <<ITEM
-{
-  "id": {"S": "1"},
-  "s3location": {"S": ${aws_s3_bucket_object.file_upload.key}"}
-}
-ITEM
-}
-
 variable "GlueCatalogDatabaseName" {
   type = string
-  default = "demogluedatabase"
+  default = "glue_database"
 }
 
 variable "CrawlerRole" {
   type = string
   default = "arn:aws:iam::061753407487:role/Glue"
-}
-
-variable "S3BucketName" {
-  type = string
 }
 
 provider "aws" {
@@ -74,6 +51,16 @@ resource "aws_glue_catalog_database" "aws_glue_catalog_database" {
   name = var.GlueCatalogDatabaseName
 }
 
+resource "aws_glue_crawler" "s3_crawler" {
+  database_name = "${aws_dynamodb_table.test_avro_data_source_index.name}"
+  name          = "s3Crawler"
+  role          = var.CrawlerRole
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.test_avro_data_source.bucket}/${aws_s3_bucket_object.file_upload.key}"
+  }
+}
+
 resource "aws_glue_crawler" "dynamo_crawler" {
   database_name = "${aws_dynamodb_table.test_avro_data_source_index.name}"
   name          = "dynamoCrawler"
@@ -81,38 +68,69 @@ resource "aws_glue_crawler" "dynamo_crawler" {
   schedule      = "cron(15 12 * * ? *)"
 
   dynamodb_target {
-    path = var.DynamoTableName
+    path = aws_dynamodb_table.test_avro_data_source_index.name
   }
 }
 
-resource "aws_glue_crawler" "example" {
-  database_name = "${aws_dynamodb_table.test_avro_data_source_index.name}"
-  name          = "s3Crawler"
-  role          = var.CrawlerRole
+resource "aws_glue_catalog_table" "aws_glue_catalog_table_merged" {
+  name          = "merged_table"
+  database_name = aws_glue_catalog_database.aws_glue_catalog_database.name
+  table_type = "EXTERNAL_TABLE"
+  description = "table used to store the joined data"
 
-  s3_target {
-    path = "s3://${aws_s3_bucket.test_avro_data_source}/${aws_s3_bucket_object.file_upload.key}"
+  parameters = {
+    EXTERNAL              = "TRUE"
+    "parquet.compression" = "SNAPPY"
+  }
+  
+  storage_descriptor {
+    location      = "s3://${aws_s3_bucket.scala_dag.bucket}/mergedTable"
+    input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
+    
+    ser_de_info {
+      name                  = "my-stream"
+      serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+      parameters = {
+        "serialization.format" = 1
+      }
+    }
+
+    columns {
+      name = "\"${aws_dynamodb_table.test_avro_data_source_index.hash_key}\""
+      type = "string"
+    }
+
+    columns {
+      name = "\"${aws_dynamodb_table.test_avro_data_source_index.range_key}\""
+      type = "string"
+    }
   }
 }
 
 data "aws_glue_script" "scala_script" {
   language = "SCALA"
 
-  dag_node {
-    id        = "datasourceDynamo"
-    node_type = "DataSource"
-
-    args {
-      name  = "database"
-      value = "\"${aws_glue_catalog_database.aws_glue_catalog_database.name}\""
-    }
-
-    args {
-      name  = "table_name"
-      value = "\"${aws_dynamodb_table.test_avro_data_source_index.name}\""
-    }
+  dag_edge {
+    source = "datasource_dynamo"
+    target = "joinDynamoAndS3"
   }
 
+  dag_edge {
+    source = "datasourceS3"
+    target = "joinDynamoAndS3"
+  }
+
+  dag_edge {
+    source = "joinDynamoAndS3"
+    target = "resolvechoice"
+  }
+
+  dag_edge {
+    source = "resolvechoice"
+    target = "datasink"
+  }
+  
   dag_node {
     id        = "datasourceS3"
     node_type = "DataSource"
@@ -149,7 +167,7 @@ data "aws_glue_script" "scala_script" {
 
     args {
       name = "keys2"
-      value = "[\"${aws_dynamodb_table.test_avro_data_source_index.hash_key}\", \"${aws_dynamodb_table.test_avro_data_source_index.range_key}\"]"
+      value = "[\"${aws_dynamodb_table.test_avro_data_source_index.hash_key}\", \"s3location\"]"
     }
   }
   
@@ -164,27 +182,27 @@ data "aws_glue_script" "scala_script" {
 
     args {
       name  = "database"
-      value = "\"${aws_glue_catalog_database.aws_glue_catalog_database_destination.name}\""
+      value = "\"${aws_glue_catalog_database.aws_glue_catalog_database.name}\""
     }
 
     args {
       name  = "table_name"
-      value = "\"dest\""
+      value = "\"${aws_glue_catalog_table.aws_glue_catalog_table_merged.name}\""
     }
   }
 
   dag_node {
-    id        = "datasink5"
+    id        = "datasink"
     node_type = "DataSink"
 
     args {
       name  = "database"
-      value = "\"${aws_glue_catalog_database.aws_glue_catalog_database_destination.name}\""
+      value = "\"${aws_glue_catalog_database.aws_glue_catalog_database.name}\""
     }
 
     args {
       name  = "table_name"
-      value = "\"${aws_glue_catalog_table.aws_glue_catalog_table_destination.name}\""
+      value = "\"${aws_glue_catalog_table.aws_glue_catalog_table_merged.name}\""
     }
   }
 }
@@ -205,4 +223,31 @@ resource "aws_s3_bucket_object" "file_upload" {
   source = "${local_file.scala_code.filename}"
 }
 
+resource "aws_cloudformation_stack" "glue_job_stack" {
+  name = "glue-job-stack"
+
+  template_body = <<STACK
+     {
+       "Resources": {
+          "GlueJob": {
+            "Type" : "AWS::Glue::Job",
+            "Properties" : {
+                "Name" :"joinData",
+                "GlueVersion": "1.0",
+                "Command" : {
+                  "Name":"glueetl",
+                  "ScriptLocation": "s3://${aws_s3_bucket.scala_dag.bucket}/${aws_s3_bucket_object.file_upload.key}"
+                },
+                "Role" : "${var.CrawlerRole}",
+                "DefaultArguments" : {
+                  "--job-language": "scala",
+                  "--class": "GlueApp"
+                },
+                "Description" : "Glue job"      
+              }
+           } 
+         }
+     }
+STACK
+}
 
